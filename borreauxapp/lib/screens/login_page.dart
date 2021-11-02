ca22442e9ced12dc0d77af676105947fd64aaf4a
Jmccain123 <jmccai8@lsu.edu>
2021-11-02 13:46:36 -0500

    fleshed out login page with working buttons

--- borreauxapp/lib/screens/login_page.dart
+++ borreauxapp/lib/screens/login_page.dart
@@ -10,33 +10,178 @@ class LoginPage extends StatefulWidget {
 
 class _LoginPageState extends State<LoginPage> {
   @override
+  Widget buildLoginButton() {
+    return Column(
+      children: <Widget>[
+        ElevatedButton(
+          child: Text('Login'),
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => nav_bar()),
+            );
+          },
+        ),
+      ],
+    );
+    //Image.asset('assets/images/4330logo3.png'),
+  }
+
+  Widget buildEmailTextbox() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Text('Email', style: TextStyle(color: Colors.white)),
+        SizedBox(height: 10.0),
+        Container(
+          alignment: Alignment.centerLeft,
+          height: 60.0,
+          decoration: new BoxDecoration(
+            border: Border.all(color: Colors.blueAccent),
+            borderRadius: BorderRadius.all(Radius.circular(20)),
+          ),
+          child: TextField(
+            keyboardType: TextInputType.emailAddress,
+            style: TextStyle(
+              color: Colors.white,
+              fontFamily: 'OpenSans',
+            ),
+            decoration: InputDecoration(
+              border: InputBorder.none,
+              contentPadding: EdgeInsets.only(top: 14.0),
+              prefixIcon: Icon(
+                Icons.email,
+                color: Colors.white,
+              ),
+              hintText: 'Enter your Email',
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget buildPasswordTextbox() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Text('Password',
+            style: TextStyle(
+              color: Colors.white,
+            )),
+        SizedBox(height: 10.0),
+        Container(
+          alignment: Alignment.centerLeft,
+          height: 60.0,
+          decoration: new BoxDecoration(
+            border: Border.all(color: Colors.blueAccent),
+            borderRadius: BorderRadius.all(Radius.circular(20)),
+          ),
+          child: TextField(
+            obscureText: true,
+            style: TextStyle(
+              color: Colors.white,
+              fontFamily: 'OpenSans',
+            ),
+            decoration: InputDecoration(
+              border: InputBorder.none,
+              contentPadding: EdgeInsets.only(top: 14.0),
+              prefixIcon: Icon(
+                Icons.lock,
+                color: Colors.white,
+              ),
+              hintText: 'Enter your Password',
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget buildForgotPasswordBtn() {
+    return Container(
+      alignment: Alignment.centerRight,
+      child: TextButton(
+        onPressed: () => print('Forgot Password Button Pressed'),
+        child: Text(
+          'Forgot Password?',
+          style: TextStyle(color: Colors.white),
+        ),
+      ),
+    );
+  }
+
+  Widget buildSignupButton() {
+    return Container(
+      alignment: Alignment.centerLeft,
+      child: TextButton(
+        onPressed: () => print('Sign up Button Pressed'),
+        child: Text(
+          'Sign up',
+          style: TextStyle(
+            color: Colors.white,
+          ),
+        ),
+      ),
+    );
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Stack(
         children: <Widget>[
-          Container(
-            width: double.infinity,
-            height: double.infinity,
-            decoration: BoxDecoration(
-              color: AppColor.primaryColor,
-            ),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: <Widget>[
-                ElevatedButton(
-                  child: Text('Open route'),
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => nav_bar()),
-                    );
-                  },
+          Stack(
+            children: <Widget>[
+              //The background color/design
+              Container(
+                height: double.infinity,
+                width: double.infinity,
+                decoration: BoxDecoration(
+                  color: Colors.blue[400],
                 ),
-              ],
-            ),
+              ),
+              Container(
+                height: double.infinity,
+                //in case entire screen is not visible on a different device
+                child: SingleChildScrollView(
+                  physics: AlwaysScrollableScrollPhysics(),
+                  padding: EdgeInsets.symmetric(
+                    horizontal: 40.0,
+                    vertical: 120.0,
+                  ),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Text(
+                        'Sign In',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontFamily: 'OpenSans',
+                          fontSize: 30.0,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      SizedBox(height: 30.0),
+                      SizedBox(
+                        height: 30.0,
+                      ),
+                      buildEmailTextbox(),
+                      buildPasswordTextbox(),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: <Widget>[
+                          buildSignupButton(),
+                          buildForgotPasswordBtn(),
+                        ],
+                      ),
+                      buildLoginButton(),
+                    ],
+                  ),
+                ),
+              )
+            ],
           ),
-          //Image.asset('assets/images/4330logo3.png'),
         ],
       ),
     );
@@ -76,3 +221,5 @@ class _LoginPageState extends State<LoginPage> {
                   MaterialPageRoute(builder: (context) => SecondPage()),
                 ),
   */
+
+//Image.asset('assets/images/4330logo3.png'),
