fe67fc79b9182c2e227dca05c1a630cd16c8462a
Chris Vasquez <56667015+chrispvasquez@users.noreply.github.com>
2021-11-20 16:39:21 -0600

    Updated main.dart to Call Proper Function for SF

--- borreauxapp/lib/widgets/nav_bar.dart
+++ borreauxapp/lib/widgets/nav_bar.dart
@@ -16,7 +16,7 @@ class nav_bar extends StatefulWidget {
 class _nav_bar_state extends State<nav_bar> {
   int _currentIndex = 0;
   final List _children = [
-    StorefrontWidget(Colors.white),
+    StorefrontState(),
     BookmarkWidget(Colors.deepOrange),
     AddWidget(Colors.green),
     MessagesWidget(Colors.blue),
