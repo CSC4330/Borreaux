0e05fbc8bcf58323b191bd79d53855e049d12bcb
Chris Vasquez <56667015+chrispvasquez@users.noreply.github.com>
2021-11-29 23:02:06 -0600

    Delete 1

--- borreauxapp/lib/model/messagesUser.dart
+++
@@ -1,8 +0,0 @@
-class MessagesUsers {
-  final String name;
-  final String messageText;
-  final String imagePath;
-  final String time;
-
-  const MessagesUsers({this.name, this.messageText, this.imagePath, this.time});
-}
