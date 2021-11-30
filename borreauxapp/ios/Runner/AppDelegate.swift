3897707d8e473a170d3b823b0b4b609470f97331
Chris Vasquez <56667015+chrispvasquez@users.noreply.github.com>
2021-11-30 00:34:22 -0600

    Fixed iOS map ?

--- borreauxapp/ios/Runner/AppDelegate.swift
+++ borreauxapp/ios/Runner/AppDelegate.swift
@@ -8,7 +8,7 @@ import GoogleMaps
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    GMSServices.provideAPIKey("your App Id")
+    GMSServices.provideAPIKey("AIzaSyCDl8B7r5F2h45mb4NG3pSZti3HqYSDPX4")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
